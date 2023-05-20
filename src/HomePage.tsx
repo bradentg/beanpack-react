efe3238a70f30d44d8a8219bbf8a45c4ef206ebb
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2023-05-19 22:48:37 -0400

    HomePage creation

---
+++ src/HomePage.tsx
@@ -0,0 +1,12 @@
+import './HomePage.css'
+
+export const HomePage = () => {
+    return (
+        <div>
+            <h1>BeanPack: The Website</h1>
+            <h3>Here it is</h3>
+            <h5>Yep, this is the one</h5>
+            <p>It's coming along</p>
+        </div>
+    )
+}
